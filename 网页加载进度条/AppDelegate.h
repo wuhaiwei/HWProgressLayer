87674614719e0ec96d28d8b6199768b68263f895
wuhaiwei <wuhaiwei0809@163.com>
2016-09-08 17:00:17 +0800

    Initial Commit

---
+++ 网页加载进度条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网页加载进度条
+//
+//  Created by  wuhiwi on 16/9/8.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网页加载进度条/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网页加载进度条
+//
+//  Created by  wuhiwi on 16/9/8.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
